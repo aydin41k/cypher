52f7383256b97f0a41df0f566c617602effe85f1
Aydin <aydin4ik@gmail.com>
2017-01-24 23:43:35 +1100

    Comments added

--- main_script.js
+++ main_script.js
@@ -1,6 +1,21 @@
+/*
+* Encoding:
+*   Calculate how many times the passphrase will be stirred and XOR'ed against the text as the fourth root of the length of the message +1
+*   Stir the passphrase - cut the first char and join it to the end
+*   Build the key - repeat the edited passphrase to match the length of the text
+*   XOR the key against the text; return the result
+*   Grab the passphrase edited in step 2 and the result from step 4; repeat steps 2-4 as many times as set in step 1
+*
+* Decoding:
+*   Magic.
+*/
+
 'use strict';
 var encoding_rounds = 4;
 
+/*
+** Build a key with a length equal to the initial text
+*/
 var build_cyph = function(pass, length) {
 	var cyph = '';
 	for(var i=0; i<length; i++) {
@@ -10,13 +25,21 @@ var build_cyph = function(pass, length) {
 	return cyph;
 }
 
+/*
+** Encode
+*/
+
 var cypher = function(message, cyph) {
+
+//Calculate how many times the passphrase will be stirred
 	encoding_rounds = Math.floor(Math.sqrt(Math.sqrt(message.length)))+1;
-	console.log('encoding '+encoding_rounds+' times.');
+
 	for( var j=0; j < encoding_rounds; j++ ) {
+//Build a key by sending the first char to the
 		cyph = cyph.slice(1) + cyph.slice(0,1);
-		console.log('cyph '+j+'. '+cyph);
 		var key = build_cyph(cyph, message.length);		
+
+//XOR the chars one-by-one		
 		for( var i=0, xor = ''; i<message.length; i++ ) {
 			var c = message.charCodeAt(i);
 			var d = key.charCodeAt(i);
@@ -25,13 +48,19 @@ var cypher = function(message, cyph) {
 			xor += next_char_xor;
 		}
 	}
+//Voila!	
 	return xor;
 }
+
+/*
+** Decode - reversing the above
+*/
 
 var reverse_xor = function(gibberish,pass) {
 	encoding_rounds = Math.floor(Math.sqrt(Math.sqrt(gibberish.length)))+1;	
 	for( var j=0; j<encoding_rounds; j++ )	{
 		var decyphered = '';
+//Redundant piece below, to be fixed
 		pass = pass.slice(1) + pass.slice(0,1);
 		console.log('pass '+j+'. '+pass);		
 		var key = build_cyph(pass, gibberish.length);	
